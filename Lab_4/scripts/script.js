e86486ab96de0619b088c8bf0f8b14d2f4235726
Nikita <nikita_isaichenk@ro.ru>
2022-12-16 04:54:27 +0700

    /

--- Lab_4/scripts/script.js
+++ Lab_4/scripts/script.js
@@ -1,80 +1,39 @@
+var addSpace = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
 function generateTree(elem) {
-    var child = elem.firstElementChild;
-    var ul = document.createElement('ul');
-    while(child) {
-        var li = document.createElement('li');
-        li.innerText = '<'+child.tagName+'>';
-        var list = generateTree(child);
-        li.appendChild(list);
-        ul.appendChild(li);
-        child = child.nextElementSibling;
-    }
-
-    var place = document.getElementById('tree');
-    place.appendChild(ul);
-    return ul;
+    var tree = findChild(elem, "", "");
+    document.getElementById("tree").innerHTML += tree;
 }
 
-function generateElements(el){
-    var div_1 = document.createElement('div');
-    var div_2 = document.createElement('div');
-    var div_3 = document.createElement('div');
-    var div_4 = document.createElement('div');
-    var form = document.createElement('form');
-    var button = document.createElement('input');
-    var span_1 = document.createElement('span');
-    var span_2 = document.createElement('span');
-    var span_3 = document.createElement('span');
+function findChild(element, space, text)
+{
+    var count = element.childNodes.length;
 
-    span_1.innerText = 'div_1';
-    span_2.innerText = 'div_2';
-    span_3.innerText = 'div_3';
+    if (element.nodeName == "#text")
+    {
 
-    form.appendChild(button);
-    form.style.height = '5%';
-    form.style.alignContent = 'center';
+        if (element.data.trim().replace("\n") != "") 
+        {
+            text = text + "<br>" + space + element.data;
+        }
+    }
+    else
+    {
+        text = text + "<br>" + space + element.nodeName;
+    }
 
-    button.type = 'button';
-    button.value = 'Переход на форму';
-    button.style.marginLeft = '50%';
+    if (count == 0)
+    {
+        return text;
+    }
+    else if (count > 0)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            text = findChild(element.childNodes[i], space+addSpace, text);
+        }
 
-    // Стрелочная функция
-    button.onclick = () => {open("../pages/forms.html", '_self')};
-
-    div_4.id = 'tree';
-    div_4.style.height = '45%';
-    div_4.style.fontSize = '13px';
-    div_4.className = 'creating_div';
-
-
-    el.appendChild(div_4);
-    el.appendChild(form);
-    el.appendChild(div_1);
-
-    div_3.appendChild(span_3);
-    
-    div_2.appendChild(div_3);
-    div_2.appendChild(span_2);
-
-    div_1.appendChild(div_2);
-    div_1.appendChild(span_1);
-    
-    div_1.id='div_1';
-    div_1.style.height = '50%';
-    div_1.style.border = '1px solid black';
-    div_1.style.margintop = '50%';
-    div_1.style.backgroundColor = 'red'
-    div_1.className = 'creating_div';
-
-    div_2.id='div_2';
-    div_2.style.height = '66%'
-    div_2.style.backgroundColor = 'blue';
-    div_2.className = 'creating_div';
-
-    div_3.id='div_3';
-    div_3.style.backgroundColor = 'white';
-    div_3.style.height = '50%';
-    div_3.className = 'creating_div';
+        return text;
+    }
 }
 
 function changeSex(obj){
@@ -133,4 +92,8 @@ function Validation(obj){
     else{
         obj.style.backgroundColor = 'green';
     }
+}
+
+function openForm(){
+    open("../pages/forms.html", "_self");
 }
